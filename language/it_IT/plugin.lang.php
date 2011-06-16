77733030f08fefa15e66e411fac1e968f4bfe3a5
riopwg <riopwg@piwigo.org>
2011-06-16 21:44:28 +0000

    language IT for SmartAlbums
    git-svn-id: http://piwigo.org/svn/extensions/SmartAlbums@11402 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['%d photos associated to album &laquo;%s&raquo;'] = '%d foto associate all\'album "%s"';
+$lang['No filter selected'] = 'Nessun filtro selezionato';
+$lang['No tag selected'] = 'Nessun tag selezionato';
+$lang['Date string is malformed'] = 'La data non è correta';
+$lang['Limit must be an integer'] = 'Il limite deve essere un numero intero';
+$lang['You can\'t use more than one limit'] = 'Potete usare un solo limite';
+$lang['All these tags'] = 'Tutti i tag';
+$lang['One of these tags'] = 'Uno dei tag';
+$lang['None of these tags'] = 'Nessun tag';
+$lang['Only these tags'] = 'Solo i tag';
+$lang['Added on'] = 'Aggiunto il';
+$lang['Added before'] = 'Aggiunto prima del';
+$lang['Added after'] = 'Aggiunto dopo il';
+$lang['Created on'] = 'Creato il';
+$lang['Created before'] = 'Creato prima del';
+$lang['Created after'] = 'Creato dopo il';
+$lang['%d photo'] = '%d foto';
+$lang['%d photos'] = '%d foto';
+$lang['tags filter'] = 'filtrare per tag';
+$lang['date filter'] = 'filtrare per data';
+$lang['limit filter'] = 'numero massimo di elementi';
+$lang['This album is a SmartAlbum'] = 'Questo album è uno SmartAlbum';
+$lang['Count'] = 'Comptare';
+$lang['Regenerate photos list of this SmartAlbum'] = 'Rigenerare le foto di questo SmartAlbum';
+$lang['Regenerate photos list of all SmartAlbums'] = 'Rigenerare le foto di tutti i SmartAlbums';
+$lang['Update albums on file upload'] = 'Aggiornare gli album dopo il caricamento di un file';
+
+?>
