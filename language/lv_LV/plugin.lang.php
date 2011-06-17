58b3bd2af8e25d95d1c9abb8247dc44003adf644
ddtddt <ddtddt@piwigo.org>
2011-06-17 10:11:02 +0000

    [extensions] - SmartAlbums - add lv_LV (Latvian) thanks to Aivars Baldone
    git-svn-id: http://piwigo.org/svn/extensions/SmartAlbums@11413 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+
+$lang['%d photos associated to album «%s»'] = '%d fotogrāfiju saistītas ar albūmu «%s»';
+$lang['No filter selected'] = 'Nav izvēlēts neviens no filtriem';
+$lang['No tag selected'] = 'Nav izvēlēts neviens no tagiem';
+$lang['Date string is malformed'] = 'Datuma rinda ir kļūdaina';
+$lang['Limit must be an integer'] = 'Limitam jābūt veselam skaitlim';
+$lang['You can\'t use more than one limit'] = 'Jūs nevarat lietot vairāk kā vienu limitu';
+$lang['All these tags'] = 'Visi šie tagi';
+$lang['One of these tags'] = 'Viens no šien tagiem';
+$lang['None of these tags'] = 'Neviens no šiem tagiem';
+$lang['Only these tags'] = 'Tikai šie tagi';
+$lang['Added on'] = 'Pievienots';
+$lang['Added before'] = 'Pievienots pirms';
+$lang['Added after'] = 'Pievienots pēc';
+$lang['Created on'] = 'Izveidots';
+$lang['Created before'] = 'Izveidots pirms';
+$lang['Created after'] = 'Izveidots pēc';
+$lang['%d photo'] = '%d foto';
+$lang['%d photos'] = '%d foto';
+$lang['tags filter'] = 'tagu filtrs';
+$lang['date filter'] = 'datuma filtrs';
+$lang['limit filter'] = 'limita filtrs';
+$lang['Add filters here'] = 'Pievienot filtrus šeit';
+$lang['This album is a SmartAlbum'] = 'Šis albūms ir SmartAlbum';
+$lang['Count'] = 'Skaits';
+$lang['Regenerate photos list of this SmartAlbum'] = 'Reģenerēt šī SmartAlbum fotogrāfiju sarakstu';
+$lang['Regenerate photos list of all SmartAlbums'] = 'Reģenerēt visu SmartAlbum fotogrāfiju sarakstu';
+$lang['Update albums on file upload'] = 'Update albums on file upload';
+$lang['All SmartAlbums'] = 'Visi SmartAlbumi';
+$lang['Add a SmartAlbum'] = 'Pievienot SmartAlbum';
+$lang['SmartAlbum deleted'] = 'SmartAlbum izdzēsts';
+$lang['SmartAlbum added'] = 'SmartAlbum pievienots';
+$lang['Only SmartAlbums are displayed on this page'] = 'Šajā lapā attēloti tikai  SmartAlbumi ';
+$lang['To order albums please go the main albums management page'] = 'Lai kārtotu albūmus, izvēlieties albūmu pārvaldības galveno lapu';
+$lang['Don\'t show this message again'] = 'Nerādīt šo ziņojumu atkārtoti';
+
+?>
