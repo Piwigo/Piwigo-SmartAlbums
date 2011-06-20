efda8fa05ebc9e529d097f14e3b4b17cbb0b149c
riopwg <riopwg@piwigo.org>
2011-06-20 09:04:26 +0000

    Update language IT
    git-svn-id: http://piwigo.org/svn/extensions/SmartAlbums@11440 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/it_IT/plugin.lang.php
+++ language/it_IT/plugin.lang.php
@@ -21,10 +21,18 @@ $lang['%d photos'] = '%d foto';
 $lang['tags filter'] = 'filtrare per tag';
 $lang['date filter'] = 'filtrare per data';
 $lang['limit filter'] = 'numero massimo di elementi';
+$lang['Add filters here'] = 'Ajoutez des filtres ici';
 $lang['This album is a SmartAlbum'] = 'Questo album è uno SmartAlbum';
 $lang['Count'] = 'Comptare';
 $lang['Regenerate photos list of this SmartAlbum'] = 'Rigenerare le foto di questo SmartAlbum';
 $lang['Regenerate photos list of all SmartAlbums'] = 'Rigenerare le foto di tutti i SmartAlbums';
 $lang['Update albums on file upload'] = 'Aggiornare gli album dopo il caricamento di un file';
+$lang['All SmartAlbums'] = 'Tutti i SmartAlbums';
+$lang['Add a SmartAlbum'] = 'Aggiungere un SmartAlbum';
+$lang['SmartAlbum deleted'] = 'SmartAlbum aggiunto';
+$lang['SmartAlbum added'] = 'SmartAlbum cancellato';
+$lang['Only SmartAlbums are displayed on this page'] = 'Solo i SmartAlbums sono visualizzati su questa pagina';
+$lang['To order albums please go the main albums management page'] = 'Per l\'ordinamento degli album recarsi sulla pagina principale della gestione degli album';
+$lang['Don\'t show this message again'] = 'Non visualissare più questo messagio';
 
 ?>
