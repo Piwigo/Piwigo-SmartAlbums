0ae47a8c30f15d243ffd285c38cedef16fdc86b3
duke <duke@piwigo.org>
2011-07-10 09:46:04 +0000

    added de_DE
    git-svn-id: http://piwigo.org/svn/extensions/SmartAlbums@11690 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+
+$lang['%d photos associated to album %s'] = '%d Fotos mit Album %s assoziiert';
+$lang['No filter selected'] = 'Kein Filter ausgewählt';
+$lang['No tag selected'] = 'Kein Stichwort ausgewählt';
+$lang['Date string is malformed'] = 'Datumsformat ungültig';
+$lang['Limit must be an integer'] = 'Limit muss eine Ganzzahl sein';
+$lang['You can\'t use more than one limit'] = 'Sie können nur ein Limit angeben';
+$lang['All these tags'] = 'Alle angeführten Stichwörter';
+$lang['One of these tags'] = 'Eines der angeführten Stichwörter';
+$lang['None of these tags'] = 'Keines der angeführten Stichwörter';
+$lang['Only these tags'] = 'Nur diese Stichwörter';
+$lang['Added on'] = 'Hinzugefügt am';
+$lang['Added before'] = 'Hinzugefügt vor';
+$lang['Added after'] = 'Hinzugefügt nach';
+$lang['Created on'] = 'Erstellt am';
+$lang['Created before'] = 'Erstellt vor';
+$lang['Created after'] = 'Erstellt nach';
+$lang['%d photo'] = '%d Foto';
+$lang['%d photos'] = '%d Fotos';
+$lang['tags filter'] = 'Stichwortfilter';
+$lang['date filter'] = 'Datumfilter';
+$lang['limit filter'] = 'Limitfilter';
+$lang['Add filters here'] = 'Filter hier einfügen';
+$lang['This album is a SmartAlbum'] = 'Dieses Album ist ein SmartAlbum';
+$lang['Count'] = 'Anzahl';
+$lang['Regenerate photos list of this SmartAlbum'] = 'Fotoliste für dieses SmartAlbum neu generieren';
+$lang['Regenerate photos list of all SmartAlbums'] = 'Fotolist für alle SmartAlben neu generieren';
+$lang['Update albums on file upload'] = 'Alben nach Dateiupload aktualisieren';
+$lang['(can cause slowdowns on admin pages)'] = '(kann die Administrationsseiten verlangsamen)';
+$lang['All SmartAlbums'] = 'Alle SmartAlben';
+$lang['Add a SmartAlbum'] = 'Neues SmartAlbum';
+$lang['SmartAlbum deleted'] = 'SmartAlbum löschen';
+$lang['SmartAlbum added'] = 'SmartAlbum erstellt';
+$lang['Only SmartAlbums are displayed on this page'] = 'Auf dieser Seite werden nur SmartAlben angezeigt';
+$lang['To order albums please go the main albums management page'] = 'Zum Sortieren von Alben besuchen Sie bitte die Verwaltungsseite für Alben <a href="admin.php?page=cat_list"></a>';
+$lang['Don\'t show this message again'] = 'Diese Nachricht nicht mehr anzeigen';
+
+?>
