70b16d39488c188080752081e00c34379b9036ed
ddtddt <ddtddt@piwigo.org>
2012-01-29 20:41:10 +0000

    [extensions] - SmartAlbums - add cs_CZ - Česky - Czech - Thanks to webprostor.eu
    git-svn-id: http://piwigo.org/svn/extensions/SmartAlbums@12982 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+
+$lang['%d photos associated to album %s'] = '%d fotografie přiřazené k album %s';
+$lang['No filter selected'] = 'Žádný filtr nevybrán';
+$lang['No tag selected'] = 'Žádný tag nevybrán';
+$lang['Date string is malformed'] = 'Řetězec datumu je chybný';
+$lang['Limit must be an integer'] = 'Limit musí být celé číslo';
+$lang['You can\'t use more than one limit'] = 'Lze použít i více než jeden limit';
+$lang['All these tags'] = 'Všechny tyto tagy';
+$lang['One of these tags'] = 'Některý z těchto tagů';
+$lang['None of these tags'] = 'Žádný z těchto tagů';
+$lang['Only these tags'] = 'Pouze tyto tagy';
+$lang['Added on'] = 'Vloženo';
+$lang['Added before'] = 'Vloženo před';
+$lang['Added after'] = 'Vloženo po';
+$lang['Created on'] = 'Vytvořeno';
+$lang['Created before'] = 'Vytvořeno před';
+$lang['Created after'] = 'Vytvořeno po';
+$lang['%d photo'] = '%d fotka';
+$lang['%d photos'] = '%d fotky';
+$lang['tags filter'] = 'filtr tagů';
+$lang['date filter'] = 'filtr datum';
+$lang['limit filter'] = 'limit filtr';
+$lang['Add filters here'] = 'Vložte filtry zde';
+$lang['This album is a SmartAlbum'] = 'Toto album je SmartAlbum';
+$lang['Count'] = 'Počet';
+$lang['Regenerate photos list of this SmartAlbum'] = 'Regenerovat seznam fotek v tomto SmartAlbum';
+$lang['Regenerate photos list of all SmartAlbums'] = 'Regenerovat seznam fotek všech SmartAlb';
+$lang['Update albums on file upload'] = 'Aktualizvat alba při uploadu souborů';
+$lang['(can cause slowdowns on admin pages)'] = '(může způsobit zpomalení odezvy v administračním rozhraní)';
+$lang['All SmartAlbums'] = 'Všechny SmartAlba';
+$lang['Add a SmartAlbum'] = 'Přidat SmartAlbum';
+$lang['SmartAlbum deleted'] = 'SmartAlbum odstraněno';
+$lang['SmartAlbum added'] = 'SmartAlbum přidáno';
+$lang['Only SmartAlbums are displayed on this page'] = 'Pouze SmartAlba jsou zobrazeny na této stránce';
+$lang['To order albums please go the main albums management page'] = 'Pro výběr alb přejděte na seznam alb - <a href="admin.php?page=cat_list">stránka správy</a>';
+$lang['Don\'t show this message again'] = 'Ne/zobrazovat příště tuto zprávu';
+
+?>
