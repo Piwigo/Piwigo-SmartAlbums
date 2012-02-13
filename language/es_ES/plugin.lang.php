960421fadf945d92176b6618c5cde912599349eb
ddtddt <ddtddt@piwigo.org>
2012-02-13 10:39:36 +0000

    [extensions] - SmartAlbums - add es_ES - Español – Spanish - Thanks to jpr928
    git-svn-id: http://piwigo.org/svn/extensions/SmartAlbums@13129 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+
+$lang['%d photos associated to album %s'] = '%d fotos relacionadas con el álbum %s';
+$lang['No filter selected'] = 'Sin filtro seleccionado';
+$lang['No tag selected'] = 'No hay etiquetas seleccionadas';
+$lang['Date string is malformed'] = 'La fecha no es correcta';
+$lang['Limit must be an integer'] = 'El límite debe ser un entero';
+$lang['You can\'t use more than one limit'] = 'Sólo se puede utilizar un límite';
+$lang['All these tags'] = 'Todas las etiquetas';
+$lang['One of these tags'] = 'Una de las etiquetas';
+$lang['None of these tags'] = 'Ninguna etiqueta';
+$lang['Only these tags'] = 'Sólo las etiquetas';
+$lang['Added on'] = 'Añadido el';
+$lang['Added before'] = 'Añadido antes de';
+$lang['Added after'] = 'Añadido después de';
+$lang['Created on'] = 'creado el';
+$lang['Created before'] = 'Creado antes del';
+$lang['Created after'] = 'Creado después del';
+$lang['%d photo'] = '%d foto';
+$lang['%d photos'] = '%d fhotos';
+$lang['tags filter'] = 'Filtrar por etiquetas';
+$lang['date filter'] = 'filtrar por fecha';
+$lang['limit filter'] = 'numero máx. de elementos';
+$lang['Add filters here'] = 'Añadir filtros aquí';
+$lang['This album is a SmartAlbum'] = 'Este álbum es un SmartAlbum';
+$lang['Count'] = 'Contar';
+$lang['Regenerate photos list of this SmartAlbum'] = 'Regenerar las fotos de este SmartAlbum';
+$lang['Regenerate photos list of all SmartAlbums'] = 'Regenerar las fotos de todos los SmartAlbums';
+$lang['Update albums on file upload'] = 'Actualización de los álbumes después de la puesta en linea de un archivo';
+$lang['(can cause slowdowns on admin pages)'] = '(puede causar retrasos)';
+$lang['All SmartAlbums'] = 'Todos los SmartAlbums';
+$lang['Add a SmartAlbum'] = 'Añadir un SmartAlbum';
+$lang['SmartAlbum deleted'] = 'SmartAlbum eliminado';
+$lang['SmartAlbum added'] = 'SmartAlbum añadido';
+$lang['Only SmartAlbums are displayed on this page'] = 'Sólo los SmartAlbums se publican en esta página';
+$lang['To order albums please go the main albums management page'] = 'Para ordenar los álbumes por favor visite la página principal <a href="admin.php?page=cat_list">Gestión de álbumes</a>';
+$lang['Don\'t show this message again'] = 'No volver a mostrar este mensaje';
+
+?>
