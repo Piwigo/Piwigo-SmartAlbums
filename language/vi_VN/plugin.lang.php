ef4045ea2c201a14bbb378000bce06145667a3ab
ddtddt <ddtddt@piwigo.org>
2012-02-18 16:29:19 +0000

    [extensions] - SmartAlbums - add vi_VN - Tiếng Việt - Vietnamese  - Thanks to Em Ku
    git-svn-id: http://piwigo.org/svn/extensions/SmartAlbums@13233 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/vi_VN/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+
+$lang['%d photos associated to album %s'] ='Có %d ảnh liên kết với album %s';
+$lang['No filter selected'] = 'Không có bộ lọc nào được chọn';
+$lang['No tag selected'] = 'Không có thẻ nào được chọn';
+$lang['Date string is malformed'] = 'Chuỗi ngày tháng bị sai định dạng';
+$lang['Limit must be an integer'] = 'Giới hạn phải là một số nguyên';
+$lang['You can\'t use more than one limit'] = 'Bạn không thể sử dụng hơn một giới hạn';
+$lang['All these tags'] = 'Tất cả các thẻ';
+$lang['One of these tags'] = 'Một trong các thẻ';
+$lang['None of these tags'] = 'Không có thẻ nào';
+$lang['Only these tags'] = 'Chỉ những thẻ này';
+$lang['Added on'] = 'Được thêm vào';
+$lang['Added before'] = 'Được thêm trước';
+$lang['Added after'] = 'Được thêm sau khi';
+$lang['Created on'] = 'Tạo ra vào';
+$lang['Created before'] = 'Tạo ra trước';
+$lang['Created after'] = 'Tạo ra sau';
+$lang['%d photo'] = '%d ảnh';
+$lang['%d photos'] = '%d ảnh';
+$lang['tags filter'] = 'lọc thẻ';
+$lang['date filter'] = 'lọc ngày tháng';
+$lang['limit filter'] = 'lọc giới hạn';
+$lang['Add filters here'] = 'Thêm bộ lọc tại đây';
+$lang['This album is a SmartAlbum'] = 'Đây là một Album Ảnh Thông Minh';
+$lang['Count'] = 'Đếm';
+$lang['Regenerate photos list of this SmartAlbum'] = 'Khởi tạo danh sách ảnh cho album Ảnh Thông minh này';
+$lang['Regenerate photos list of all SmartAlbums'] = 'Khởi tạo danh sách ảnh cho tất cả album Ảnh Thông minh';
+$lang['Update albums on file upload'] = 'Cập nhật albums bằng cách tải tập tin lên';
+$lang['(can cause slowdowns on admin pages)'] = '(có thể làm chậm các trang quản lý)';
+$lang['All SmartAlbums'] = 'Tất cả album Ảnh Thông minh';
+$lang['Add a SmartAlbum'] = 'Thêm một album Ảnh Thông minh';
+$lang['SmartAlbum deleted'] = 'Đã xóa album Ảnh Thông minh';
+$lang['SmartAlbum added'] = 'Đã thêm album Ảnh thông minh';
+$lang['Only SmartAlbums are displayed on this page'] = 'Chỉ có album Ảnh Thông minh mới hiển thị trên trang này';
+$lang['To order albums please go the main albums management page'] = 'Để sắp xếp các albums, vui lòng  vào <a href="admin.php?page=cat_list">trang quản lý album chính</a>';
+$lang['Don\'t show this message again'] = 'Không hiển thị thông báo này nữa';
+
+?>
