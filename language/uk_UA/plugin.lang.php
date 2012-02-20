68f4b22a07d47bfbfda406345b56d9e77a3d8886
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-02-20 06:51:36 +0000

    [SmartAlbums] Update uk_UA, thanks to : animan
    git-svn-id: http://piwigo.org/svn/extensions/SmartAlbums@13263 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,58 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photo'] = '%d фотографія';
+$lang['%d photos'] = '%d фотографій';
+$lang['%d photos associated to album %s'] = '%s фотографії, пов\'язані з альбомом %s';
+$lang['(can cause slowdowns on admin pages)'] = '(може привести до уповільнення роботи на сторінках адміністратора)';
+$lang['Add a SmartAlbum'] = 'Додати смарт-альбом';
+$lang['Add filters here'] = 'Додати фільтр тут';
+$lang['Added after'] = 'Додано після';
+$lang['Added before'] = 'Додано до';
+$lang['Added on'] = 'Додано';
+$lang['All SmartAlbums'] = 'Всі смарт-альбоми';
+$lang['All these tags'] = 'Всі ці теґи';
+$lang['Count'] = 'Перерахувати';
+$lang['Created after'] = 'Створено після';
+$lang['Created before'] = 'Створено до';
+$lang['Created on'] = 'Створено';
+$lang['date filter'] = 'фільтр дати';
+$lang['Date string is malformed'] = 'Невірний порядок дати';
+$lang['Don\'t show this message again'] = 'Не показувати це повідомлення знову';
+$lang['limit filter'] = 'обмежити кількість фільтром';
+$lang['Limit must be an integer'] = 'Кількість має бути цілим числом';
+$lang['No filter selected'] = 'Вибрані без фільтру';
+$lang['No tag selected'] = 'Немає обраних теґів';
+$lang['None of these tags'] = 'Жоден з цих теґів';
+$lang['One of these tags'] = 'Один з цих теґів';
+$lang['Only SmartAlbums are displayed on this page'] = 'Тільки смарт-альбоми відображаються на цій сторінці';
+$lang['Only these tags'] = 'Тільки ці теґи';
+$lang['Regenerate photos list of all SmartAlbums'] = 'Відновіть фотографії список всіх смарт-альбомів';
+$lang['Regenerate photos list of this SmartAlbum'] = 'Відновити фотографії списку ціього смарт-альбому';
+$lang['SmartAlbum added'] = 'Смарт-альбом додано';
+$lang['SmartAlbum deleted'] = 'Смарт-альбом видалено';
+$lang['tags filter'] = 'фільтр теґв';
+$lang['This album is a SmartAlbum'] = 'Цей альбом є смарт-альбомом';
+$lang['To order albums please go the main albums management page'] = 'Щоб упорядкувати основні альбоми перейдіть на <a href="admin.php?page=cat_list">сторінку управління</a>';
+$lang['Update albums on file upload'] = 'Оновлення альбомів на завантаження файлів';
+$lang['You can\'t use more than one limit'] = 'Ви не можете використовувати більше одного';
+?>
