42d892035bdf8609561d3107bfc91627ae6893b4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-17 15:09:31 +0000

    [SmartAlbums] Add ru_RU, thanks to : nadusha
    git-svn-id: http://piwigo.org/svn/extensions/SmartAlbums@14207 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,58 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photo'] = '%d фотография';
+$lang['%d photos associated to album %s'] = '%d фотографий, связанных с альбомом %s';
+$lang['%d photos'] = '%d фотографий';
+$lang['(can cause slowdowns on admin pages)'] = '(может привести к замедлению работы на странице администрирования)';
+$lang['Add a SmartAlbum'] = 'Добавить Умный Альбом';
+$lang['Add filters here'] = 'Добавить фильтр здесь';
+$lang['Added after'] = 'Добавлено ​​после';
+$lang['Added before'] = 'Добавлено ​​до';
+$lang['Added on'] = 'Добавлено';
+$lang['All SmartAlbums'] = 'Все Умные Альбомы';
+$lang['All these tags'] = 'Все эти теги';
+$lang['Count'] = 'Количество';
+$lang['Created after'] = 'Создано после';
+$lang['Created before'] = 'Создано до';
+$lang['Created on'] = 'Создано';
+$lang['Date string is malformed'] = 'Дата написана неверно';
+$lang['Don\'t show this message again'] = 'Не показывать больше это сообщение ';
+$lang['Limit must be an integer'] = 'Предел должен быть целым числом';
+$lang['No filter selected'] = 'Фильтр не выбран';
+$lang['No tag selected'] = 'Тег не выбран';
+$lang['None of these tags'] = 'Ни один из этих тегов';
+$lang['One of these tags'] = 'Один из этих тегов';
+$lang['Only SmartAlbums are displayed on this page'] = 'На этой странице отображаются только Умные Альбомы';
+$lang['Only these tags'] = 'Только эти теги';
+$lang['Regenerate photos list of all SmartAlbums'] = 'Восстановить список фотографий всех Умных Альбомов';
+$lang['Regenerate photos list of this SmartAlbum'] = 'Восстановить список фотографий этого Умного Альбома';
+$lang['SmartAlbum added'] = 'Умный Альбом добавлен';
+$lang['SmartAlbum deleted'] = 'Умный Альбом удален';
+$lang['This album is a SmartAlbum'] = 'Этот альбом - Умный Альбом';
+$lang['To order albums please go the main albums management page'] = 'Чтобы упорядочить альбомы, перейдите к главной странице <a href="admin.php?page=cat_list">управления альбомами</a>';
+$lang['Update albums on file upload'] = 'Обновить альбомы при загрузке файлов';
+$lang['You can\'t use more than one limit'] = 'Вы не можете использовать более одного предела';
+$lang['date filter'] = 'фильтр даты';
+$lang['limit filter'] = 'фильтр предела';
+$lang['tags filter'] = 'фильтр тегов';
+?>
