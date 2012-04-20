0f968a8b5579b0017c32243aa6649665d8ff7706
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-20 15:18:30 +0000

    [SmartAlbums] Add sk_SK, thanks to : dodo
    git-svn-id: http://piwigo.org/svn/extensions/SmartAlbums@14234 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,58 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photos associated to album %s'] = '%d fotiek spojených s albumom %s';
+$lang['%d photo'] = '%d fotka';
+$lang['%d photos'] = '%d fotiek';
+$lang['(can cause slowdowns on admin pages)'] = '(môžete spôsobiť spomalenie na administrátorských stránkach)';
+$lang['Add a SmartAlbum'] = 'Pridať SmartAlbum';
+$lang['Add filters here'] = 'Pridať filtre';
+$lang['Added after'] = 'Pridané po';
+$lang['Added before'] = 'Pridané pred';
+$lang['Added on'] = 'Pridané';
+$lang['All SmartAlbums'] = 'Všetky SmartAlbumy';
+$lang['All these tags'] = 'Všetky tieto kľúčové slová';
+$lang['Count'] = 'Počet';
+$lang['Created after'] = 'Vytvorené po';
+$lang['Created before'] = 'Vytvorené pred';
+$lang['Created on'] = 'Vytvorené';
+$lang['Date string is malformed'] = 'Dátum je chybne vytvorený';
+$lang['Don\'t show this message again'] = 'Nezobrazovať viac túto správu';
+$lang['Limit must be an integer'] = 'Limit musí byť celé číslo';
+$lang['No filter selected'] = 'Žiaden filter nie je označený';
+$lang['No tag selected'] = 'Žiadne kľúčové slovo nie je označené';
+$lang['None of these tags'] = 'Žiadne z týchto kľúčových slov';
+$lang['One of these tags'] = 'Jedno z týchto kľúčových slov';
+$lang['Only SmartAlbums are displayed on this page'] = 'Len SmartAlbumy sú zobrazené na tejto stránke';
+$lang['Only these tags'] = 'Len tieto kľúčové slová';
+$lang['Regenerate photos list of all SmartAlbums'] = 'Zlepšiť zoznam fotiek vo všetkých SmartAlbumoch';
+$lang['Regenerate photos list of this SmartAlbum'] = 'Zlepšiť zoznam fotiek v tomto SmartAlbume';
+$lang['SmartAlbum added'] = 'SmartAlbum pridaný';
+$lang['SmartAlbum deleted'] = 'SmartAlbum vymazaný';
+$lang['This album is a SmartAlbum'] = 'Tento album je SmartAlbum';
+$lang['To order albums please go the main albums management page'] = 'Na zoradneie albumov choďte na hlavný album <a href="admin.php?page=cat_list">stránka správy</a>';
+$lang['Update albums on file upload'] = 'Aktualizovať albumy pri nahratí súboru';
+$lang['You can\'t use more than one limit'] = 'Nemôžete použiť viac ako jeden limit';
+$lang['date filter'] = 'filter dátumu';
+$lang['limit filter'] = 'limit filtra';
+$lang['tags filter'] = 'filter kľúčových slov';
+?>
