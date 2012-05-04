b7c67921bafd2208ba35b773f3c6a2d1e29ebf2a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-04 20:10:23 +0000

    [smartalbums] Update ca_ES, thanks to : AlbertParera
    git-svn-id: http://piwigo.org/svn/extensions/SmartAlbums@14751 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,58 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photo'] = '%d imatge';
+$lang['%d photos associated to album %s'] = '%d imatges associades a l\'àlbum %s';
+$lang['%d photos'] = '%d imatges';
+$lang['(can cause slowdowns on admin pages)'] = '(Pot causar retards en les pàgines d\'administració)';
+$lang['Add a SmartAlbum'] = 'Afegir un SmartAlbum';
+$lang['Add filters here'] = 'Afegir filtres aquí';
+$lang['Added after'] = 'Afegit després de';
+$lang['Added before'] = 'Afegit abans de';
+$lang['Added on'] = 'Afegit el';
+$lang['All SmartAlbums'] = 'Tots els SmartAlbums';
+$lang['All these tags'] = 'Totes aquestes etiquetes';
+$lang['Count'] = 'Comptar';
+$lang['Created after'] = 'Creat després de';
+$lang['Created before'] = 'Creat abans de';
+$lang['Created on'] = 'Creat el';
+$lang['Date string is malformed'] = 'La cadena de caracters que representa la data és incorrecte';
+$lang['Don\'t show this message again'] = 'No tornar a mostrar aquest missatge de nou';
+$lang['Limit must be an integer'] = 'El límit ha de ser un nombre enter';
+$lang['No filter selected'] = 'Sense filtre seleccionat';
+$lang['No tag selected'] = 'Sense etiqueta seleccionada';
+$lang['None of these tags'] = 'Cap d\'aquestes etiquetes';
+$lang['One of these tags'] = 'Una d\'aquestes etiquetes';
+$lang['Only SmartAlbums are displayed on this page'] = 'En aquesta pàgina només es mostre els SmartAlbums';
+$lang['Only these tags'] = 'Només aquestes etiquetes';
+$lang['Regenerate photos list of all SmartAlbums'] = 'Regenerar llista d\'imatges de tots els SmartAlbums';
+$lang['Regenerate photos list of this SmartAlbum'] = 'Regenerar llista d\'imatges d\'aquest SmartAlbum';
+$lang['SmartAlbum added'] = 'SmartAlbum afegit';
+$lang['SmartAlbum deleted'] = 'El SmartAlbum ha sigut eliminat';
+$lang['This album is a SmartAlbum'] = 'Aquest àlbum és un SmartAlbum';
+$lang['To order albums please go the main albums management page'] = 'Per ordenar els àlbums cal fer-ho des de la pàgina d\'administració principal dels àlbums <a href="admin.php?page=cat_list"></a>';
+$lang['Update albums on file upload'] = 'Actualitzar àlbums al pujar els arxius';
+$lang['You can\'t use more than one limit'] = 'No es pot utilitzar més d\'un límit';
+$lang['date filter'] = 'Filtre per data';
+$lang['limit filter'] = 'Filtre per límit';
+$lang['tags filter'] = 'Filtre per etiquetes';
+?>
