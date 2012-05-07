e4dc80a2a6b54e400aab420abd09ececdd984029
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-07 07:44:29 +0000

    [smartalbums] Add pt_PT, thanks to : klito
    git-svn-id: http://piwigo.org/svn/extensions/SmartAlbums@14822 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_PT/plugin.lang.php
@@ -0,0 +1,58 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photo'] = '%d foto';
+$lang['%d photos associated to album %s'] = '%d fotos associadas ao album %s';
+$lang['%d photos'] = '%d fotos';
+$lang['(can cause slowdowns on admin pages)'] = '(pode causar lentidão nas páginas da Admin)';
+$lang['Add a SmartAlbum'] = 'Adicionar um album Smart';
+$lang['Add filters here'] = 'Adicionar filtros aqui';
+$lang['Added after'] = 'Adicionado depois';
+$lang['Added before'] = 'Adicionado antes';
+$lang['Added on'] = 'Adcionado em';
+$lang['All SmartAlbums'] = 'Todos os albuns Smart';
+$lang['All these tags'] = 'Todas estas etiquetas';
+$lang['Count'] = 'Conta';
+$lang['Created after'] = 'Criado depois';
+$lang['Created before'] = 'Criado antes';
+$lang['Created on'] = 'Criado em';
+$lang['Date string is malformed'] = 'Conjunto data incorrecto';
+$lang['Don\'t show this message again'] = 'Não mosstrar esta mensagem de novo';
+$lang['Limit must be an integer'] = 'Limite deve ser numero integro';
+$lang['No filter selected'] = 'Filtro não seleccionado';
+$lang['No tag selected'] = 'Etiqueta não seleccionada';
+$lang['None of these tags'] = 'Nenhuma destas etiquetas';
+$lang['One of these tags'] = 'Uma destas etiquetas';
+$lang['Only SmartAlbums are displayed on this page'] = 'Apenas albuns Smart são mostrados nesta página';
+$lang['Only these tags'] = 'Apennas estas etiquetas';
+$lang['Regenerate photos list of all SmartAlbums'] = 'Regenerar fotos em todos os albuns Smart';
+$lang['Regenerate photos list of this SmartAlbum'] = 'Regenerar fotos neste album Smart';
+$lang['SmartAlbum added'] = 'Adicionado album Smart';
+$lang['SmartAlbum deleted'] = 'Album Smart foi apagado';
+$lang['This album is a SmartAlbum'] = 'Este album é um album Smart';
+$lang['To order albums please go the main albums management page'] = 'Para ordenar albuns, vá para a página de grenciação dos albuns <a href="admin.php?page=cat_list">';
+$lang['Update albums on file upload'] = 'Actualizar albuns com carga de arquivos';
+$lang['You can\'t use more than one limit'] = 'Não pode usar mais que um limite';
+$lang['date filter'] = 'Filtro data';
+$lang['limit filter'] = 'Filtro limite';
+$lang['tags filter'] = 'Filtro etiquetas';
+?>
