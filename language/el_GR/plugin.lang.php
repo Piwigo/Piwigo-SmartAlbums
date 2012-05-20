d0e5e1be6c1ca1f688e45cd18d8c32f4e8b451db
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-20 19:03:31 +0000

    [smartalbums] Update el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/extensions/SmartAlbums@15152 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photo'] = '%d φωτογραφία';
+$lang['%d photos associated to album %s'] = '%d φωτογραφίες που σχετίζονται με το άλμπουμ %s';
+$lang['Added after'] = 'Προστέθηκε μετά από';
+$lang['Added before'] = 'Προστέθηκε πριν';
+$lang['Added on'] = 'Προστέθηκε στις';
+$lang['All these tags'] = 'Όλες αυτές οι ετικέτες';
+$lang['Created after'] = 'Δημιουργήθηκε μετά από';
+$lang['Created before'] = 'Δημιουργήθηκε πριν από';
+$lang['Created on'] = 'Δημιουργήθηκε στις';
+$lang['Date string is malformed'] = 'Η Ημερομηνία δεν έχει σωστή μορφή';
+$lang['Limit must be an integer'] = 'Το Όριο πρέπει να είναι ακέραιος';
+$lang['No filter selected'] = 'Δεν έχει επιλεγεί φίλτρο';
+$lang['No tag selected'] = 'Δεν έχει επιλεγεί ετικέτα';
+$lang['None of these tags'] = 'Καμία από αυτές τις ετικέτες';
+$lang['One of these tags'] = 'Μία από αυτές τις ετικέτες';
+$lang['Only these tags'] = 'Μόνο αυτές οι λέξεις-κλειδιά';
+$lang['You can\'t use more than one limit'] = 'Δεν μπορείτε να χρησιμοποιήσετε περισσότερα από ένα όριο';
+?>
