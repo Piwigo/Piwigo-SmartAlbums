9c097df0512ac76dbf1d8b0360fdd2ec5741d4b7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-19 09:55:16 +0000

    [smartalbums] Add hu_HU, thanks to : samli
    git-svn-id: http://piwigo.org/svn/extensions/SmartAlbums@16841 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/hu_HU/plugin.lang.php
@@ -0,0 +1,79 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photo'] = '%d kép';
+$lang['%d photos associated to album %s'] = '%d kép társítva a(z) %s albumhoz';
+$lang['%d photos'] = '%d kép';
+$lang['(can cause slowdowns)'] = '(lassulást okozhat)';
+$lang['Above'] = 'Felett';
+$lang['Add filters here'] = 'Adjon hozzá szűrőt!';
+$lang['Added after'] = 'Hozzáadva után';
+$lang['Added before'] = 'Hozzáadva előtt';
+$lang['Added on'] = 'Hozzáadva';
+$lang['All SmartAlbums'] = 'Összes dinamikus album';
+$lang['All these tags'] = 'Ezen címkék mindegyike';
+$lang['Author is empty'] = 'Nincs szerző kiválasztva';
+$lang['Begins with'] = 'Ezzel kezdődik';
+$lang['Bellow'] = 'Alatt';
+$lang['Contains'] = 'Tartalmazza';
+$lang['Count'] = 'Számol';
+$lang['Created after'] = 'Létrehozva után';
+$lang['Created before'] = 'Létrehozva előtt';
+$lang['Date string is malformed'] = 'Dátum karakterlánc hibás';
+$lang['Doesn\'t begin with'] = 'Nem ezzel kezdődik';
+$lang['Doesn\'t contain'] = 'Nem tartalmazza';
+$lang['Doesn\'t end with'] = 'Nem ezzel végződik';
+$lang['Don\'t show this message again'] = 'Ne jelenjen meg többé ez az üzenet';
+$lang['Ends with'] = 'Ezzel végződik';
+$lang['Filters'] = 'Szűrők';
+$lang['For "Is (not) in", separate each author by a comma'] = 'A "(Ne) Tartalmazza" szűrők esetében a szerzőket vesszővel kell elválasztani.';
+$lang['Hits must be an integer'] = 'Találatok egész szám legyen!';
+$lang['Is in'] = 'Tartalmazza';
+$lang['Is not in'] = 'Ne tartalmazza';
+$lang['Is not'] = 'Ne legyen benne';
+$lang['Is'] = 'Legyen benne';
+$lang['Limit must be an integer'] = 'A szint egész szám legyen!';
+$lang['Name is empty'] = 'A név üres';
+$lang['No filter selected'] = 'Nincs szűrő kiválasztva';
+$lang['No tag selected'] = 'Nincs címke kiválasztva';
+$lang['None of these tags'] = 'Ezen címkék egyike sem';
+$lang['One of these tags'] = 'Ezen címkék egyike';
+$lang['Only SmartAlbums are displayed on this page'] = 'Ezen az oldalon csak a dinamikus albumok jelennek meg';
+$lang['Only these tags'] = 'Csak ezek a címkék';
+$lang['Regenerate photos list of all SmartAlbums'] = 'Képek listájának frissítése az összes dinamikus albumban';
+$lang['Regenerate photos list of this SmartAlbum'] = 'Képek listájának frissítése a dinamikus albumban';
+$lang['SmartAlbum added'] = 'Dinamikus album létrehozva';
+$lang['SmartAlbum deleted'] = 'Dinamikus album törölve';
+$lang['This album is a SmartAlbum'] = 'Ez az album egy dinamikus album';
+$lang['To order albums please go the main albums <a href="%s">management page</a>'] = 'Albumok kezeléséhez lépjen az Albumok, <a href="%s">Kezelés oldalra</a>';
+$lang['Update albums on file upload'] = 'Albumok frissítése képek feltöltésekor';
+$lang['You can\'t use more than one level filter'] = 'Nem használható egyszerre egynél több szintű szűrő';
+$lang['You can\'t use more than one limit filter'] = 'Nem használható egyszerre egynél több határérték szűrő';
+$lang['author_filter'] = 'Szerző';
+$lang['create a new SmartAlbum'] = 'Új dinamikus album létrehozása';
+$lang['date_filter'] = 'Dátum';
+$lang['hit_filter'] = 'Találatok száma';
+$lang['level_filter'] = 'Hozzáférési szint';
+$lang['limit_filter'] = 'Képek max. száma';
+$lang['name_filter'] = 'Kép neve';
+$lang['tags_filter'] = 'Címkék';
+?>
