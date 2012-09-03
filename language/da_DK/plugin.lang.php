942c42b76590b6b91e224fa007a3d0b9a6c9d9ca
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-03 11:49:48 +0000

    [smartalbums] Update da_DK, thanks to : thomas1956
    git-svn-id: http://piwigo.org/svn/extensions/SmartAlbums@17707 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,83 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['SmartAlbums are considered private for everyone, and a user can see it\'s content only if available in another album he has access to.'] = 'SmartAlbums er private for den enkelte bruger og denne kan kun se indholdet, hvis dette er indeholdt i et album som han har adgang til.';
+$lang['To order albums please go the main albums <a href="%s">management page</a>'] = 'For at sortere albums, vælg <a href="%s">siden for administration af kategorier</a>';
+$lang['Regenerate photos list of all SmartAlbums'] = 'Gendan liste over fotos i alle SmartAlbums';
+$lang['Regenerate photos list of this SmartAlbum'] = 'Gendan liste over fotos i dette SmartAlbum';
+$lang['SmartAlbum deleted'] = 'SmartAlbum slettet';
+$lang['This album is a SmartAlbum'] = 'Dette er et SmartAlbum';
+$lang['Update albums on file upload'] = 'Opdater albums ved upload af fil';
+$lang['You can\'t use more than one level filter'] = 'Du kan ikke anvende mere end et niveau filter';
+$lang['You can\'t use more than one limit filter'] = 'Du kan ikke anvende mere end et begrænsende filter';
+$lang['can cause slowdowns'] = 'kan nedsætte performance';
+$lang['create a new SmartAlbum'] = 'Opret et nyt SmartAlbum';
+$lang['limit_filter'] = 'Max. antal fotos';
+$lang['None of these tags'] = 'Ingen af disse tags';
+$lang['One of these albums'] = 'Et af disse albums';
+$lang['One of these tags'] = 'Et af disse tags';
+$lang['Only SmartAlbums are displayed on this page'] = 'Kun SmartAlbums vises på denne side';
+$lang['Only these albums'] = 'Kun disse albums';
+$lang['Only these tags'] = 'Kun disse tags';
+$lang['Rating score must be an integer'] = 'Vurdering skal angives som et heltal';
+$lang['SmartAlbum added'] = 'SmartAlbum tilføjet';
+$lang['Doesn\'t contain'] = 'Indeholder ikke';
+$lang['Doesn\'t end with'] = 'Ender ikke med';
+$lang['Don\'t show this message again'] = 'Vis ikke denne besked igen';
+$lang['Ends with'] = 'Ender med';
+$lang['Exclude SmartAlbums from permissions management'] = 'Ekskluder SmartAlbums fra adgangsstyring';
+$lang['Filters'] = 'Filtre';
+$lang['For "Is (not) in", separate each author by a comma'] = 'For "Indeholder (ikke)", adskil hver fotograf med et komma';
+$lang['Hits'] = 'Hits';
+$lang['Hits must be an integer'] = 'Hits skal angives som et heltal';
+$lang['Is'] = 'Indeholder';
+$lang['Is in'] = 'Indeholdt i';
+$lang['Is not'] = 'Indeholder ikke';
+$lang['Is not in'] = 'Er ikke indeholdt i';
+$lang['Limit must be an integer'] = 'Max/min skal angives som et heltal';
+$lang['Max. number of photos'] = 'Max. antal fotos';
+$lang['Name is empty'] = 'Navn er ikke angivet';
+$lang['No album selected'] = 'Der er ikke valgt et album';
+$lang['No filter selected'] = 'Der er ikke valgt et filter';
+$lang['No tag selected'] = 'Der er ikke valgt et tag';
+$lang['None of these albums'] = 'Intet af disse albums';
+$lang['Created after'] = 'Oprettet efter';
+$lang['Created before'] = 'Oprettet før';
+$lang['Date string is malformed'] = 'Angivelse af dato er ikke korrekt';
+$lang['Doesn\'t begin with'] = 'Starter ikke med';
+$lang['All SmartAlbums'] = 'Alle SmartAlbums';
+$lang['All these tags'] = 'Alle disse tags';
+$lang['Author is empty'] = 'Forfatter er ikke angivet ';
+$lang['Begins with'] = 'Starter med';
+$lang['Bellow'] = 'Under';
+$lang['Contains'] = 'Indeholder';
+$lang['Count'] = 'Antal';
+$lang['%d photos associated to album %s'] = '%d fotos knyttet til album %s';
+$lang['All these albums'] = 'Alle disse albums';
+$lang['%d photo'] = '%d foto';
+$lang['%d photos'] = '%d fotos';
+$lang['Above'] = 'Ovenover';
+$lang['Add filters here'] = 'Tilføj filtre her';
+$lang['Added after'] = 'Tilføjet efter';
+$lang['Added before'] = 'Tilføjet før';
+$lang['Added on'] = 'Tilføjet den';
+?>
