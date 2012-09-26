8f0e99e3dcaad91c397db73c0f92817ee048b97c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-26 19:21:45 +0000

    [smartalbums] Update pt_BR, thanks to : oigreslima
    git-svn-id: http://piwigo.org/svn/extensions/SmartAlbums@18279 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,83 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['None of these tags'] = 'Nenhuma dessas tags';
+$lang['One of these albums'] = 'Um dessas categorias';
+$lang['One of these tags'] = 'Uma dessas tags';
+$lang['Only SmartAlbums are displayed on this page'] = 'Somente SmartAlbums são mostrados nessa página';
+$lang['Only these albums'] = 'Somente essa categoria';
+$lang['Only these tags'] = 'Somente essa tag';
+$lang['Rating score must be an integer'] = 'Nota de avaliação deve ser um inteiro';
+$lang['Regenerate photos list of all SmartAlbums'] = 'Gere novamente lista de todas as fotos do SmartAlbums';
+$lang['Regenerate photos list of this SmartAlbum'] = 'Gere novamente lita desse SmartAlbum';
+$lang['SmartAlbum added'] = 'SmartAlbum adicionado';
+$lang['SmartAlbum deleted'] = 'SmartAlbum apagado';
+$lang['SmartAlbums are considered private for everyone, and a user can see it\'s content only if available in another album he has access to.'] = 'SmartAlbums são considerados privados para todos e um usuário pode ver seu conteúdo somente se avaliável em outra categoria que ele tenha acesso.';
+$lang['This album is a SmartAlbum'] = 'Essa categoria é um SmartAlbum';
+$lang['To order albums please go the main albums <a href="%s">management page</a>'] = 'Para ordenar categorias vá para a categoria principal na <a href="%s">Página de gerenciamento</a>';
+$lang['Update albums on file upload'] = 'Atualize categorias enviando arquivo';
+$lang['You can\'t use more than one level filter'] = 'Você não pode usar mais do que um filtro de nível.';
+$lang['You can\'t use more than one limit filter'] = 'Você não pode usar mais do que um limite e filtro.';
+$lang['can cause slowdowns'] = 'pode causar lentidão';
+$lang['create a new SmartAlbum'] = 'Cria um novo SmartAlbum';
+$lang['limit_filter'] = 'Número máximo de fotos';
+$lang['Doesn\'t contain'] = 'Não contém';
+$lang['Doesn\'t end with'] = 'Não termina com';
+$lang['Don\'t show this message again'] = 'Não mostrar essa mensagem novamente';
+$lang['Ends with'] = 'Termina com';
+$lang['Exclude SmartAlbums from permissions management'] = 'Exclua SmartAlbums do gerenciador de permissões';
+$lang['Filters'] = 'Filtros';
+$lang['For "Is (not) in", separate each author by a comma'] = 'Para "(não) está em", separe cada autor por um vírgula';
+$lang['Hits'] = 'Hits';
+$lang['Hits must be an integer'] = 'Hits deve ser um inteiro';
+$lang['Is'] = 'É';
+$lang['Is in'] = 'Está em';
+$lang['Is not'] = 'Não é';
+$lang['Is not in'] = 'Não está em';
+$lang['Limit must be an integer'] = 'Limite deve ser um inteiro';
+$lang['Max. number of photos'] = 'Número máximo de fotos';
+$lang['Name is empty'] = 'Nome está vazio';
+$lang['No album selected'] = 'Nenhuma categoria selecionada';
+$lang['No filter selected'] = 'Nenhum filtro selecionado';
+$lang['No tag selected'] = 'Nenhuma tag selecionada';
+$lang['None of these albums'] = 'Nenhuma dessas categorias';
+$lang['%d photo'] = '%d fotos';
+$lang['%d photos'] = '%d Fotos';
+$lang['%d photos associated to album %s'] = '%d fotos associadas às categorias %s';
+$lang['Above'] = 'Acima';
+$lang['Add filters here'] = 'adicione filtros aqui';
+$lang['Added after'] = 'adicione depois';
+$lang['Added before'] = 'adicione antes';
+$lang['Added on'] = 'adicionado';
+$lang['All SmartAlbums'] = 'Todos as categorias inteligentes';
+$lang['All these albums'] = 'Todos essas categorias';
+$lang['All these tags'] = 'Todas essas tags';
+$lang['Author is empty'] = 'Autor está vazio.';
+$lang['Begins with'] = 'Começa com';
+$lang['Bellow'] = 'Pertence';
+$lang['Contains'] = 'Contém';
+$lang['Count'] = 'Conta';
+$lang['Created after'] = 'Criado depois';
+$lang['Created before'] = 'Criado antes';
+$lang['Date string is malformed'] = 'Data está mal formada';
+$lang['Doesn\'t begin with'] = 'Não inicia com';
+?>
