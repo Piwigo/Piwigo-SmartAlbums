31b67a35024d0a34dce0f2d4346bf5a442d3af7f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-10-18 05:39:45 +0000

    [smartalbums] Add zh_CN, thanks to : foddyy
    git-svn-id: http://piwigo.org/svn/extensions/SmartAlbums@18686 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,57 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Doesn\'t contain'] = '不包含';
+$lang['Doesn\'t end with'] = '不结束于';
+$lang['Don\'t show this message again'] = '不再显示此信息';
+$lang['Ends with'] = '结束于';
+$lang['Exclude SmartAlbums from permissions management'] = '将SmartAlbums从权限管理中排除';
+$lang['Filters'] = '过滤器';
+$lang['Hits'] = '点击数';
+$lang['Hits must be an integer'] = '点击数必须为整数';
+$lang['Limit must be an integer'] = '限制必须为整数';
+$lang['Max. number of photos'] = '相片最大数量';
+$lang['Name is empty'] = '名称为空';
+$lang['No album selected'] = '未选择相册';
+$lang['No filter selected'] = '未选择过滤器';
+$lang['No tag selected'] = '未选择标签';
+$lang['%d photo'] = '%d相片';
+$lang['%d photos'] = '%d相片';
+$lang['%d photos associated to album %s'] = '%d相片关联到%s相册';
+$lang['Above'] = '关于';
+$lang['Add filters here'] = '在此添加过滤器';
+$lang['Added after'] = '在此之后添加';
+$lang['Added before'] = '在此之前添加';
+$lang['Added on'] = '添加在';
+$lang['All SmartAlbums'] = '所有智能相册';
+$lang['All these albums'] = '所有这些相册';
+$lang['All these tags'] = '所有这些标签';
+$lang['Author is empty'] = '作者为空';
+$lang['Begins with'] = '开始于';
+$lang['Bellow'] = '以下';
+$lang['Contains'] = '包含';
+$lang['Count'] = '计数';
+$lang['Created after'] = '在此之后创建';
+$lang['Created before'] = '在此之前创建';
+$lang['Date string is malformed'] = '日期字符串有缺陷';
+$lang['Doesn\'t begin with'] = '不开始于';
+?>
