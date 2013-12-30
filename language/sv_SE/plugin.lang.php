d6c19ae3e67da052728b5cc06358383351dc1303
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-12-30 06:31:13 +0000

    [smartalbums] Update sv_SE, thanks to : magnus
    git-svn-id: http://piwigo.org/svn/extensions/SmartAlbums@26356 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,82 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Only SmartAlbums are displayed on this page'] = 'Bara SmartAlbum visas på denna sidan';
+$lang['Rating score must be an integer'] = 'Bedömningspoäng måste anges som heltal';
+$lang['Regenerate photos list of all SmartAlbums'] = 'Återskapa listan på foton från alla SmartAlbum';
+$lang['Regenerate photos list of this SmartAlbum'] = 'Återskapa listan på foton i detta SmartAlbum';
+$lang['You can\'t use more than one limit filter'] = 'Du kan inte använda mer än ett begränsningsfilter';
+$lang['can cause slowdowns'] = 'Kan orsaka seghet';
+$lang['create a new SmartAlbum'] = 'Skapa ett nytt SmartAlbum';
+$lang['None of these tags'] = 'Ingen av dessa taggar';
+$lang['One of these albums'] = 'Ett av dessa album';
+$lang['One of these tags'] = 'En av dessa taggar';
+$lang['Only these albums'] = 'Bara dessa album';
+$lang['Only these tags'] = 'Bara dessa taggar';
+$lang['Regex is malformed'] = 'Felaktigt regex';
+$lang['Regular expression'] = 'Regular expression';
+$lang['SmartAlbum added'] = 'SmartAlbum tillaggt';
+$lang['SmartAlbum deleted'] = 'SmartAlbum borttaget';
+$lang['This album is a SmartAlbum'] = 'Detta album är ett SmartAlbum';
+$lang['Update albums every %s days'] = 'Uppdatera album varje %s dagar';
+$lang['Update albums on file upload'] = 'Uppdatera album vid varje fil uppladdning';
+$lang['You can\'t use more than one level filter'] = 'Du kan inte använda mer än en nivå filter';
+$lang['Doesn\'t contain'] = 'Innehåller inte';
+$lang['Doesn\'t end with'] = 'Slutar inte med';
+$lang['Don\'t show this message again'] = 'Visa inte detta meddelande igen';
+$lang['Ends with'] = 'Slutar med';
+$lang['Exclude SmartAlbums from permissions management'] = 'Exkludera SmartAlbums från behörighetssystemet';
+$lang['Filters'] = 'Filter';
+$lang['Hits'] = 'Träffar';
+$lang['Hits must be an integer'] = 'Träffar måste anges som heltal';
+$lang['Is'] = 'Är';
+$lang['Is in'] = 'Är i';
+$lang['Is not'] = 'Är inte';
+$lang['Is not in'] = 'Är inte i';
+$lang['Limit must be an integer'] = 'Gränsen måste vara ett heltal';
+$lang['Max. number of photos'] = 'Max antal foton';
+$lang['Name is empty'] = 'Namnet är tomt';
+$lang['No album selected'] = 'Inget album valt';
+$lang['No filter selected'] = 'Inget filter valt';
+$lang['No tag selected'] = 'Ingen tag vald';
+$lang['None of these albums'] = 'Inget av dessa album';
+$lang['Add filters here'] = 'Lägg till filter här';
+$lang['Added after'] = 'Lades till efter';
+$lang['Added before'] = 'Lades till före';
+$lang['Added on'] = 'Lades till på';
+$lang['All SmartAlbums'] = 'Alla SmartAlbums';
+$lang['All these albums'] = 'Alla dessa album';
+$lang['All these tags'] = 'Alla dessa taggar';
+$lang['Author is empty'] = 'Ingen författare';
+$lang['Begins with'] = 'Börjar med';
+$lang['Bellow'] = 'Nedanför';
+$lang['Contains'] = 'Innehåller';
+$lang['Count'] = 'Antal';
+$lang['Created after'] = 'Skapad efter';
+$lang['Created before'] = 'Skapad före';
+$lang['Date string is malformed'] = 'Ogiltig datum sträng';
+$lang['Doesn\'t begin with'] = 'Börjar inte med';
+$lang['Above'] = 'Ovanför';
+$lang['%d photos'] = '%d foton';
+$lang['%d photo'] = '%d foto';
+$lang['%d photos associated to album %s'] = '%d foton associerade med album %s';
+?>
