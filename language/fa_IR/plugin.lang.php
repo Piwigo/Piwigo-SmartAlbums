c9b37824a1896089e6af1c5f9e5d01299cea5ce4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-14 20:23:08 +0000

    [smartalbums] Update fa_IR, thanks to : ipic
    git-svn-id: http://piwigo.org/svn/extensions/SmartAlbums@26709 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fa_IR/plugin.lang.php
@@ -0,0 +1,46 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photos associated to album %s'] = '%d تصاویر مرتبط به آلبوم%s ';
+$lang['Above'] = 'بالا';
+$lang['Add filters here'] = 'افزود پالایش دراینجا';
+$lang['Added after'] = 'افزودن بعد از';
+$lang['Added before'] = 'افزودن قبل از ';
+$lang['Added on'] = 'افزودن در';
+$lang['All SmartAlbums'] = 'تمام البوم های هوش مند';
+$lang['Is not'] = 'این نیست';
+$lang['Is not in'] = 'این نیست در';
+$lang['Mode'] = 'مدل';
+$lang['Name is empty'] = 'نام خالی است';
+$lang['No album selected'] = 'البومی انتخاب نشد';
+$lang['No filter selected'] = 'هیچ پالایشی انتخاب نشده';
+$lang['No tag selected'] = 'هیچ برچسبی انتخاب شده';
+$lang['Ends with'] = 'به پایان می رسد با';
+$lang['Exclude SmartAlbums from permissions management'] = 'محروم کردن SmartAlbums از مدیریت مجوز ها';
+$lang['For "Is (not) in", separate each author by a comma'] = 'برای "باشد (نه) در" از هم جدا هر نویسنده توسط یک کاما از هم';
+$lang['Hits must be an integer'] = 'شماره نامعتبر روز';
+$lang['Invalid number of days'] = 'بازدید باید یک عدد صحیح باشد';
+$lang['Is in'] = 'هست در';
+$lang['Filters'] = 'پالایش';
+$lang['Hits'] = 'باز خورد';
+$lang['Is'] = 'هست';
+?>
