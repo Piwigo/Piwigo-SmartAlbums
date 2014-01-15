b829e061e08271972eb6a0642d6c935a82040f04
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-15 20:48:23 +0000

    [smartalbums] Update et_EE, thanks to : Pelagis
    git-svn-id: http://piwigo.org/svn/extensions/SmartAlbums@26766 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/et_EE/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['All these albums'] = 'Kõik need albumid';
+$lang['All these tags'] = 'Kõik need märksõnad';
+$lang['Author is empty'] = 'Autor on määramata';
+$lang['Begins with'] = 'Algab ';
+$lang['Bellow'] = 'All';
+$lang['Contains'] = 'Sisaldab';
+$lang['Count'] = 'Loend';
+$lang['Created after'] = 'Loodud pärast';
+$lang['Created before'] = 'Loodud enne';
+$lang['Date string is malformed'] = 'Kuupäeva kirje on valesti';
+$lang['Doesn\'t begin with'] = 'Ei alga ';
+$lang['Doesn\'t contain'] = 'Ei sisalda';
+$lang['Doesn\'t end with'] = 'Ei lõpe ';
+$lang['%d photos associated to album %s'] = '%d fotot on seotud albumiga %s';
+$lang['Above'] = 'Ülal';
+$lang['Add filters here'] = 'Lisa filtrid siin';
+$lang['Added after'] = 'Lisatud pärast';
+$lang['Added before'] = 'Lisatud enne';
+$lang['Added on'] = 'Lisatud kuupäeval';
+$lang['All SmartAlbums'] = 'Kõik dünaamilised albumid';
+?>
