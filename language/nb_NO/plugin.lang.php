9f5e20ab408a5837cc98f89b09496ff821393604
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-10-14 19:37:27 +0000

    [smartalbums] Update nb_NO, thanks to : paulen
    git-svn-id: http://piwigo.org/svn/extensions/SmartAlbums@30060 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nb_NO/plugin.lang.php
@@ -0,0 +1,88 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['You can\'t use more than one limit filter'] = 'Du kan ikke bruke mer enn et begrenset filter';
+$lang['can cause slowdowns'] = 'kan føre til forsinkelser';
+$lang['create a new SmartAlbum'] = 'opprette en ny SmartAlbum';
+$lang['Update albums every %s days'] = 'Oppdater albumene hver %s dag';
+$lang['Update albums on file upload'] = 'Oppdater albumene når fil opplastes';
+$lang['You can\'t use more than one level filter'] = 'Du kan ikke bruke mere enn et nivå filter';
+$lang['To order albums please go the main albums <a href="%s">management page</a>'] = 'For å bestille albumer vennligst gå til hoved album <a href="%s">management page</a>';
+$lang['This album is a SmartAlbum'] = 'Dette albumet er et SmartAlbum';
+$lang['The sort order is only used in addition to the limit filter, it does not impact the final display order'] = 'Sorteringsrekkefølgen er kun brukt i tillegg til et begrenset filter, det påvirker ikke den endelige visningsrekkefølgen';
+$lang['SmartAlbums are considered private for everyone, and a user can see it\'s content only if available in another album he has access to.'] = 'SmartAlbums anses private for alle, og en bruker kan se innholdet kun hvis det er tilgjengelig i et annet album han har tilgang til.';
+$lang['SmartAlbum deleted'] = 'SmartAlbum slettet';
+$lang['SmartAlbum added'] = 'SmartAlbum lagt til';
+$lang['Select albums...'] = 'Velg albumer...';
+$lang['Regular expression'] = 'Regulært utrykk';
+$lang['Regex is malformed'] = 'Regulært utrykk er ugyldig';
+$lang['Date string is malformed'] = 'Dato tekst er ugyldig';
+$lang['None of these tags'] = 'Ingen av disse tagger';
+$lang['One of these albums'] = 'En av disse albumene';
+$lang['One of these tags'] = 'En av disse taggene';
+$lang['Only SmartAlbums are displayed on this page'] = 'Kun SmartAlbums bli vist på denne siden';
+$lang['Only these albums'] = 'Kun disse albumene';
+$lang['Only these tags'] = 'Kun disse taggene';
+$lang['Photos must match all filters'] = 'Bilde må samsvare med alle filter';
+$lang['Photos must match at least one filter'] = 'Bilde må samsvare med minst ett filter';
+$lang['Rating score must be an integer'] = 'Karakter må være et heltall';
+$lang['Regenerate photos list of all SmartAlbums'] = 'Fornyer bilde liste over alle SmartAlbums';
+$lang['Regenerate photos list of this SmartAlbum'] = 'Fornyer bilde liste over dette SmartAlbums';
+$lang['Don\'t show this message again'] = 'Ikke vis denne beskjeden igjen';
+$lang['Ends with'] = 'Slutter med';
+$lang['Exclude SmartAlbums from permissions management'] = 'Ekskluder SmartAlbums fra administrasjons tillatelser';
+$lang['Filters'] = 'Filtere';
+$lang['For "Is (not) in", separate each author by a comma'] = 'For "Er(ikke) i", skiller hver forfatter med komma';
+$lang['Hits'] = 'Treff';
+$lang['Hits must be an integer'] = 'Treff må være et heltall';
+$lang['Invalid number of days'] = 'Ugyldig antall dager';
+$lang['Is'] = 'Er';
+$lang['Is in'] = 'Er i';
+$lang['Is not'] = 'Er ikke';
+$lang['Is not in'] = 'Er ikke i';
+$lang['Limit must be an integer'] = 'Grensen må være et heltall';
+$lang['Max. number of photos'] = 'Maks antall bilder';
+$lang['Name is empty'] = 'Navn er tomt';
+$lang['No album selected'] = 'Ingen album er valgt';
+$lang['No filter'] = 'Ingen filter';
+$lang['No filter selected'] = 'Ingen filter er valgt';
+$lang['No tag selected'] = 'Ingen tag er valgt';
+$lang['None of these albums'] = 'Ingen av disse albumer';
+$lang['%d photos associated to album %s'] = '%d bilder knyttet til album %s';
+$lang['Above'] = 'Over';
+$lang['Add filters here'] = 'Legg til filter her';
+$lang['Added after'] = 'Lagt til etter';
+$lang['Added before'] = 'Lagt til før';
+$lang['Added on'] = 'Lagt på';
+$lang['All SmartAlbums'] = 'Alle SmartAlbums';
+$lang['All these albums'] = 'Alle disse albumene';
+$lang['All these tags'] = 'Alle disse taggene';
+$lang['Author is empty'] = 'Forfatter er tom';
+$lang['Begins with'] = 'Begynn med';
+$lang['Bellow'] = 'nedenfor';
+$lang['Contains'] = 'Inneholder';
+$lang['Count'] = 'Tell';
+$lang['Created after'] = 'Opprettet etter';
+$lang['Created before'] = 'Opprettet før';
+$lang['Doesn\'t begin with'] = 'Begynner ikke med';
+$lang['Doesn\'t contain'] = 'Inneholder ikke';
+$lang['Doesn\'t end with'] = 'Ender ikke med';
