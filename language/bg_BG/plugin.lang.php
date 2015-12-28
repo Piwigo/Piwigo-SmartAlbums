9cf6cf037acb4503648ecc1e3935ccd307743992
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-12-28 19:10:09 +0100

    Add bg_BG, thanks to turnokopa

---
+++ language/bg_BG/plugin.lang.php
@@ -0,0 +1,51 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Count'] = 'Брой';
+$lang['All SmartAlbums'] = 'Всички SmartAlbums';
+$lang['All these albums'] = 'Всички тези албуми';
+$lang['All these tags'] = 'Всички тези тагове';
+$lang['Author is empty'] = 'Автор не е зададен';
+$lang['Hits'] = 'Посещения';
+$lang['None of these albums'] = 'Никой от тези албуми';
+$lang['No tag selected'] = 'Няма избран таг';
+$lang['No filter selected'] = 'Няма избран филтър';
+$lang['No filter'] = 'Няма филтър';
+$lang['One of these albums'] = 'Един от тези албуми';
+$lang['Select albums...'] = 'Избор на албум...';
+$lang['This album is a SmartAlbum'] = 'Този албум е SmartAlbum';
+$lang['SmartAlbum added'] = 'SmartAlbum е добавен';
+$lang['SmartAlbum deleted'] = 'SmartAlbum е изтрит';
+$lang['Is not'] = 'Не е';
+$lang['Is not in'] = 'Не е в';
+$lang['Is'] = 'Е';
+$lang['Is in'] = 'Е в';
+$lang['Filters'] = 'Филтри';
+$lang['Ends with'] = 'Завършване с';
+$lang['Above'] = 'Относно';
+$lang['Created before'] = 'Създадено преди';
+$lang['Created after'] = 'Създадено след';
+$lang['Begins with'] = 'Започва с';
+$lang['Added before'] = 'Добавено преди';
+$lang['Added on'] = 'Добавено на';
+$lang['Added after'] = 'Добавено след';
+$lang['Add filters here'] = 'Добави филтри тук';
