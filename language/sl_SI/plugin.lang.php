1155a0188775147e13588c016ac101295172f67a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-12-07 07:02:30 +0100

    Update sl_SI, thanks to maxima

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,73 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['You can\'t use more than one limit filter'] = 'Ne morete uporabiti več kot enega omejitvenega filtra';
+$lang['You can\'t use more than one level filter'] = 'Ne morete uporabiti več kot enega nivojskega filtra';
+$lang['Regex is malformed'] = 'Regex je napačno oblikovan';
+$lang['Invalid number of days'] = 'Neveljavno število dni';
+$lang['For "Is (not) in", separate each author by a comma'] = 'Za "Je (ni) v" vsakega avtorja ločite z vejico';
+$lang['Count'] = 'Preštej';
+$lang['Rating score must be an integer'] = 'Ocenjevalni rezultat mora biti celo število';
+$lang['Limit must be an integer'] = 'Omejitev mora biti celo število';
+$lang['Hits must be an integer'] = 'Zadetki morajo biti celo število';
+$lang['Date string is malformed'] = 'Datumski niz je napačno oblikovan';
+$lang['No album selected'] = 'Noben album ni izbran';
+$lang['No tag selected'] = 'Nobena oznaka ni izbrana';
+$lang['Doesn\'t end with'] = 'Se ne konča z';
+$lang['Doesn\'t begin with'] = 'Se ne začne z';
+$lang['Doesn\'t contain'] = 'Ne vsebuje';
+$lang['Ends with'] = 'Konča z';
+$lang['Begins with'] = 'Začne z';
+$lang['Regular expression'] = 'Vsakdanje izražanje';
+$lang['Hits'] = 'Zadetkov';
+$lang['Author is empty'] = 'Avtor je prazen';
+$lang['All these albums'] = 'Vsi ti albumi';
+$lang['%d photos associated to album %s'] = '%d slike, povezane z albumom %s';
+$lang['Is not in'] = 'Ni v';
+$lang['Is not'] = 'Ni';
+$lang['Is in'] = 'Je v';
+$lang['Is'] = 'Je';
+$lang['Contains'] = 'Vsebuje';
+$lang['Created before'] = 'Ustvarjeno pred';
+$lang['Created after'] = 'Ustvarjeno po';
+$lang['None of these albums'] = 'Nobenega od teh albumov';
+$lang['Only these albums'] = 'Samo ti albumi';
+$lang['One of these albums'] = 'Eden od teh albumov';
+$lang['Added before'] = 'Dodano pred';
+$lang['Added after'] = 'Dodano po';
+$lang['Added on'] = 'Dodano na';
+$lang['No filter selected'] = 'Noben filter ni izbran';
+$lang['Only these tags'] = 'Samo te oznake';
+$lang['None of these tags'] = 'Nobena od teh oznak';
+$lang['One of these tags'] = 'Ena od teh oznak';
+$lang['All these tags'] = 'Vse te oznake';
+$lang['Name is empty'] = 'Ime je prazno';
+$lang['Max. number of photos'] = 'Maks. število slik';
+$lang['Above'] = 'Zgoraj';
+$lang['Bellow'] = 'Spodaj';
+$lang['No filter'] = 'Ni filtra';
+$lang['Photos must match at least one filter'] = 'Slike se morajo ujemati z vsaj enim filtrom';
+$lang['Photos must match all filters'] = 'Slike se morajo ujemati z vsemi filtri';
+$lang['Add filters here'] = 'Tukaj dodajte filtre';
+$lang['SmartAlbum deleted'] = 'SmartAlbum je izbrisan';
+$lang['SmartAlbum added'] = 'SmartAlbum je dodan';
+$lang['This album is a SmartAlbum'] = 'Ta album je SmartAlbum';
+$lang['The sort order is only used in addition to the limit filter, it does not impact the final display order'] = 'Vrstni red razvrščanja se uporablja samo poleg filtra za omejitve in ne vpliva na končni vrstni red prikaza';
+$lang['Select albums...'] = 'Izberite albume...';
+$lang['Update albums every %s days'] = 'Posodobi albume vsakih %s dni';
+$lang['Filters'] = 'Filtri';
+$lang['Exclude SmartAlbums from permissions management'] = 'Izključite SmartAlbume iz upravljanja dovoljenj';
+$lang['can cause slowdowns'] = 'lahko povzroči upočasnitev';
+$lang['Update albums on file upload'] = 'Posodobite albume ob nalaganju datoteke';
+$lang['Regenerate photos list of this SmartAlbum'] = 'Obnovite seznam slik tega SmartAlbuma';
+$lang['Regenerate photos list of all SmartAlbums'] = 'Obnovite seznam slik vseh SmartAlbumov';
+$lang['create a new SmartAlbum'] = 'ustvari novi SmartAlbum';
+$lang['All SmartAlbums'] = 'Vsi SmartAlbumi';
+$lang['Don\'t show this message again'] = 'Tega sporočila ne prikaži več';
+$lang['To order albums please go the main albums <a href="%s">management page</a>'] = 'Če želite naročiti albume, pojdite na <a href="%s">stran za upravljanje</a> glavnih albumov';
+$lang['Only SmartAlbums are displayed on this page'] = 'Na tej strani so prikazani samo SmartAlbumi';
+$lang['SmartAlbums are considered private for everyone, and a user can see it\'s content only if available in another album he has access to.'] = 'SmartAlbumi veljajo za zasebne za vsakogar in uporabnik lahko vidi njihovo vsebino le, če je na voljo v drugem albumu, do katerega ima dostop.';
