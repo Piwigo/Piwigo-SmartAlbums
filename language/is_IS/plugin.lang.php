273d1178a847ae9077edf97278ba802c7e5577b2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-04-29 06:57:38 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%d photos associated to album %s'] = '%d myndir sem tengjast albúmi %s';
+$lang['Above'] = 'Fyrir ofan';
+$lang['Add filters here'] = 'Bættu við síum hér';
+$lang['Added after'] = 'Bætt við á eftir';
+$lang['Added before'] = 'Bætt við á undan';
+$lang['Added on'] = 'Bætt við þann';
+$lang['All SmartAlbums'] = 'Öll snjallalbúm';
+$lang['All these albums'] = 'Öll þessi albúm';
+$lang['All these tags'] = 'Öll þessi merki';
+$lang['Author is empty'] = 'Höfundarreitur er tómur';
+$lang['Begins with'] = 'Byrjar með';
+$lang['Bellow'] = 'Belgur';
+$lang['Contains'] = 'Inniheldur';
+$lang['Count'] = 'Telja';
+$lang['Created after'] = 'Stofnað eftir';
+$lang['Created before'] = 'Stofnað fyrir';
+$lang['Date string is malformed'] = 'Dagsetningarstrengur er rangur';
+$lang['Doesn\'t begin with'] = 'Byrjar ekki með';
+$lang['Doesn\'t contain'] = 'Inniheldur ekki';
+$lang['Doesn\'t end with'] = 'Endar ekki með';
