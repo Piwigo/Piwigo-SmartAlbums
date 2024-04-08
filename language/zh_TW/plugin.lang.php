5333db320e408cedc9e99fd2c7014270225ed7dd
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-04-08 10:50:56 +0200

    Update zh_TW, thanks to closer

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Select albums...'] = '選擇相簿...';
+$lang['Rating score must be an integer'] = '評分分數必須為整數';
+$lang['Regex is malformed'] = 'Regex 格式錯誤';
+$lang['Regular expression'] = '正規表達式 (Regex)';
+$lang['Doesn\'t begin with'] = '並未以此開頭';
+$lang['Doesn\'t contain'] = '並未包含';
+$lang['Doesn\'t end with'] = '並未以此結尾';
+$lang['Ends with'] = '以此結尾';
+$lang['Begins with'] = '以此開頭';
+$lang['Contains'] = '包含';
+$lang['Created before'] = '於此日前建立';
+$lang['Created after'] = '於此日後建立';
+$lang['Added on'] = '於此日上傳';
+$lang['Added before'] = '於此日前上傳';
+$lang['Added after'] = '於此日後上傳';
+$lang['All SmartAlbums'] = '所有的智慧相簿';
+$lang['All these tags'] = '需擁有以下所有標籤';
+$lang['Only these tags'] = '需完全符合以下的標籤組合';
+$lang['One of these tags'] = '需擁有以下任一個標籤';
+$lang['None of these tags'] = '不得擁有以下任何標籤';
+$lang['Photos must match at least one filter'] = '照片只需符合至少一項篩選條件';
+$lang['Photos must match all filters'] = '照片必須符合所有篩選條件';
+$lang['This album is a SmartAlbum'] = '此相簿為智慧相簿';
+$lang['SmartAlbum deleted'] = '成功刪除智慧相簿';
+$lang['SmartAlbum added'] = '成功新增智慧相簿';
+$lang['You can\'t use more than one level filter'] = '你只能只用一層的篩選條件';
+$lang['You can\'t use more than one limit filter'] = '您只能使用一個限制篩選條件';
+$lang['can cause slowdowns'] = '可能會影響執行效率';
+$lang['create a new SmartAlbum'] = '建立新的智慧相簿';
+$lang['Update albums on file upload'] = '當檔案上傳時更新相簿內容';
+$lang['Update albums every %s days'] = '每 %s 天更新相簿內容';
