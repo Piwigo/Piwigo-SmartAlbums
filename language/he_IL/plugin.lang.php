8437f6a67234d25e7f45f40fde7dc0695875e03f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 16:09:45 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,73 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%d photos associated to album %s'] = '%d תמונות הקשורות לאלבום %s';
+$lang['Above'] = 'מעל';
+$lang['Add filters here'] = 'הוסף סננים כאן';
+$lang['Added after'] = 'נוסף אחרי';
+$lang['Added before'] = 'נוסף לפני';
+$lang['Added on'] = 'נוסף ב-';
+$lang['All SmartAlbums'] = 'כל ה-SmartAlbums';
+$lang['All these albums'] = 'כל האלבומים הללו';
+$lang['All these tags'] = 'כל התגיות הללו';
+$lang['Author is empty'] = 'המחבר ריק';
+$lang['Begins with'] = 'מתחיל עם';
+$lang['Bellow'] = 'מתחת';
+$lang['Contains'] = 'מכיל';
+$lang['Count'] = 'ספירה';
+$lang['Created after'] = 'נוצר אחרי';
+$lang['Created before'] = 'נוצר לפני';
+$lang['Date string is malformed'] = 'מחרוזת התאריך אינה תקינה';
+$lang['Doesn\'t begin with'] = 'לא מתחיל ב-';
+$lang['Doesn\'t contain'] = 'לא מכיל';
+$lang['Doesn\'t end with'] = 'לא מסתיים ב-';
+$lang['Don\'t show this message again'] = 'אל תציג את ההודעה הזו שוב';
+$lang['Ends with'] = 'מסתיים ב-';
+$lang['Exclude SmartAlbums from permissions management'] = 'אל תכלול SmartAlbums בניהול ההרשאות';
+$lang['Filters'] = 'סננים';
+$lang['For "Is (not) in", separate each author by a comma'] = 'עבור "האם (לא) נמצא ב-", פרד כל מחבר בפסיק';
+$lang['Hits'] = 'פגיעות';
+$lang['Hits must be an integer'] = 'פגיעות חייבות להיות מספר שלם';
+$lang['Invalid number of days'] = 'מספר ימים לא תקין';
+$lang['Is'] = 'הוא';
+$lang['Is in'] = 'נמצא ב-';
+$lang['Is not'] = 'אינו';
+$lang['Is not in'] = 'לא נמצא ב-';
+$lang['Limit must be an integer'] = 'המגבלה חייבת להיות מספר שלם';
+$lang['Max. number of photos'] = 'מספר מקסימלי של תמונות';
+$lang['Name is empty'] = 'השם ריק';
+$lang['No album selected'] = 'לא נבחר אלבום';
+$lang['No filter'] = 'ללא סננים';
+$lang['No filter selected'] = 'לא נבחר סנן';
+$lang['No tag selected'] = 'לא נבחרת תג';
+$lang['None of these albums'] = 'אף אלבום מהאלה';
+$lang['None of these tags'] = 'אף תג מהתגים הללו';
+$lang['One of these albums'] = 'אחד מהאלבומים הללו';
+$lang['One of these tags'] = 'אחד מהתגים הללו';
+$lang['Only SmartAlbums are displayed on this page'] = 'רק SmartAlbums מוצגים בדף זה';
+$lang['Only these albums'] = 'רק אלבומים אלו';
+$lang['Only these tags'] = 'רק תגי אלו';
+$lang['Photos must match all filters'] = 'התמונות חייבות להתאים לכל הסננים';
+$lang['Photos must match at least one filter'] = 'התמונות חייבות להתאים ללפחות סנן אחד';
+$lang['Rating score must be an integer'] = 'ציון דירוג חייב להיות מספר שלם';
+$lang['Regenerate photos list of all SmartAlbums'] = 'לשחזר את רשימת התמונות של כל ה-SmartAlbums';
+$lang['Regenerate photos list of this SmartAlbum'] = 'לשחזר את רשימת התמונות של זה SmartAlbum';
+$lang['Regex is malformed'] = 'הביטוי הרגולרי אינו תקין';
+$lang['Regular expression'] = 'ביטוי רגולרי';
+$lang['Select albums...'] = 'בחר אלבומים...';
+$lang['SmartAlbum added'] = 'SmartAlbum נוסף';
+$lang['SmartAlbum deleted'] = 'SmartAlbum נמחק';
+$lang['SmartAlbums are considered private for everyone, and a user can see it\'s content only if available in another album he has access to.'] = 'SmartAlbums נחשבים לפרטיים לכולם, ומשתמש יכול לראות את התוכן שלו רק אם הוא זמין באלבום אחר שיש לו גישה אליו.';
+$lang['The sort order is only used in addition to the limit filter, it does not impact the final display order'] = 'סדר המיון משמש רק בנוסף לסנן המגבלה, ואינו משפיע על סדר התצוגה הסופי';
+$lang['This album is a SmartAlbum'] = 'אלבום זה הוא SmartAlbum';
+$lang['To order albums please go the main albums <a href="%s">management page</a>'] = 'כדי לסדר אלבומים, עבור לעמוד ה<a href="%s">ניהול אלבומים</a> הראשי';
+$lang['Update albums every %s days'] = 'לעדכן אלבומים כל %s ימים';
+$lang['Update albums on file upload'] = 'לעדכן אלבומים בעת העלאת קובץ';
+$lang['You can\'t use more than one level filter'] = 'אי אפשר להשתמש ביותר מסנן רמה אחת';
+$lang['You can\'t use more than one limit filter'] = 'אי אפשר להשתמש ביותר מסנן הגבלה אחד';
+$lang['can cause slowdowns'] = 'עשוי לגרום להאטות';
+$lang['create a new SmartAlbum'] = 'ליצור SmartAlbum חדש';
