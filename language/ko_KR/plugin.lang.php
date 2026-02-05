cae512d27ca4db756afadcf854689086b6bdd15d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-05 06:20:31 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,69 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['SmartAlbums are considered private for everyone, and a user can see it\'s content only if available in another album he has access to.'] = 'SmartAlbums은 모든 사용자에게 비공개로 간주되며, 사용자는 자신이 접근 권한을 가진 다른 앨범에 해당 콘텐츠가 포함된 경우에만 볼 수 있습니다.';
+$lang['The sort order is only used in addition to the limit filter, it does not impact the final display order'] = '정렬 순서는 제한 필터에 추가로만 사용되며, 최종 표시 순서에는 영향을 미치지 않습니다.';
+$lang['This album is a SmartAlbum'] = '이 앨범은 SmartAlbum입니다.';
+$lang['To order albums please go the main albums <a href="%s">management page</a>'] = '앨범 정렬을 원하시면 메인 앨범 <a href="%s">관리 페이지</a>로 이동하세요.';
+$lang['Update albums every %s days'] = '앨범을 %s일마다 업데이트합니다';
+$lang['Update albums on file upload'] = '파일 업로드 시 앨범 업데이트';
+$lang['You can\'t use more than one level filter'] = '한 개 이상의 레벨 필터를 사용할 수 없습니다';
+$lang['You can\'t use more than one limit filter'] = '한 개 이상의 제한 필터를 사용할 수 없습니다';
+$lang['can cause slowdowns'] = '속도 저하를 유발할 수 있습니다';
+$lang['create a new SmartAlbum'] = '새 SmartAlbum 생성';
+$lang['None of these tags'] = '이 태그 중 아무것도 해당되지 않음';
+$lang['One of these albums'] = '이 앨범 중 하나';
+$lang['One of these tags'] = '이 태그 중 하나';
+$lang['Only SmartAlbums are displayed on this page'] = '이 페이지에는 SmartAlbums만 표시됩니다';
+$lang['Only these albums'] = '이 앨범만';
+$lang['Only these tags'] = '이 태그만';
+$lang['Photos must match all filters'] = '사진은 모든 필터와 일치해야 함';
+$lang['Photos must match at least one filter'] = '사진은 최소 한 개의 필터와 일치해야 함';
+$lang['Rating score must be an integer'] = '평점 점수는 정수여야 함';
+$lang['Regenerate photos list of all SmartAlbums'] = '모든 SmartAlbums의 사진 목록 재생성';
+$lang['Regenerate photos list of this SmartAlbum'] = '이 SmartAlbums의 사진 목록 재생성';
+$lang['Regex is malformed'] = '정규식이 잘못 구성되었습니다';
+$lang['Regular expression'] = '정규식';
+$lang['Select albums...'] = '앨범 선택...';
+$lang['SmartAlbum added'] = 'SmartAlbum 추가됨';
+$lang['SmartAlbum deleted'] = 'SmartAlbum 삭제됨';
+$lang['Don\'t show this message again'] = '이 메시지를 다시 표시하지 않음';
+$lang['Ends with'] = '끝으로';
+$lang['Exclude SmartAlbums from permissions management'] = '권한 관리에서 SmartAlbums 제외';
+$lang['Filters'] = '필터';
+$lang['For "Is (not) in", separate each author by a comma'] = '"Is (not) in"의 경우 각 작성자를 쉼표로 구분';
+$lang['Hits'] = '조회수';
+$lang['Hits must be an integer'] = '조회수는 정수여야 함';
+$lang['Invalid number of days'] = '잘못된 일수';
+$lang['Limit must be an integer'] = '제한값은 정수여야 함';
+$lang['Max. number of photos'] = '최대 사진 수';
+$lang['Name is empty'] = '이름이 비어 있음';
+$lang['No album selected'] = '앨범이 선택되지 않음';
+$lang['No filter'] = '필터 없음';
+$lang['No filter selected'] = '필터가 선택되지 않음';
+$lang['No tag selected'] = '태그가 선택되지 않음';
+$lang['None of these albums'] = '이 앨범들 중 아무것도 없음';
+$lang['%d photos associated to album %s'] = '%d장의 사진이 앨범 %s에 연결됨';
+$lang['Above'] = '위';
+$lang['Add filters here'] = '여기에 필터 추가';
+$lang['Added after'] = '추가된 날짜 이후';
+$lang['Added before'] = '추가된 날짜 이전';
+$lang['Added on'] = '추가된 날짜';
+$lang['All SmartAlbums'] = 'SmartAlbums 전체';
+$lang['All these albums'] = '이 앨범 전체';
+$lang['All these tags'] = '이 태그들 전체';
+$lang['Author is empty'] = '작성자 없음';
+$lang['Begins with'] = '시작하는 문자';
+$lang['Bellow'] = '아래';
+$lang['Contains'] = '포함하는 문자';
+$lang['Count'] = '개수';
+$lang['Created after'] = '생성일 이후';
+$lang['Created before'] = '생성일 이전';
+$lang['Date string is malformed'] = '날짜 문자열이 잘못되었습니다';
+$lang['Doesn\'t begin with'] = '시작하지 않음';
+$lang['Doesn\'t contain'] = '포함하지 않음';
+$lang['Doesn\'t end with'] = '끝나지 않음';
