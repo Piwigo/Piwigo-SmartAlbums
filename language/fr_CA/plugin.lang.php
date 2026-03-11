c08b3c149475e9b397822f4dbc6e24eb88fb6676
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-03-11 14:22:36 +0100

    Update fr_CA, thanks to bgoutier

---
+++ language/fr_CA/plugin.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['None of these tags'] = 'Aucun des mots-clés';
+$lang['One of these tags'] = 'Un des mots-clés';
+$lang['Only these tags'] = 'Seulement les mots-clés';
+$lang['Limit must be an integer'] = 'Le nombre d\'éléments doit être un entier';
+$lang['No tag selected'] = 'Aucun mot-clé sélectionné';
+$lang['All these tags'] = 'Tous les mots-clés';
